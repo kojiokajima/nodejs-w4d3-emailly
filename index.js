a0852ec9abcccac288572bc1c7363d5791ed3640
Koji Okajima <koji970124@gmail.com>
2021-05-26 13:57:11 -0700

    class done

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+require('./models/user.model')
+require('./services/passport')
+
+const keys = require('./config/keys')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(passport.initialize())
+
+const authRoutes = require('./routes/auth.route')
+
+app.use('/api/auth', authRoutes)
+
+mongoose.connect(keys.mongoURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+}).then(() => {
+  const PORT = process.env.PORT || 5000
+  app.listen(PORT, () => console.log(`Server has started running on port ${PORT}`))
+}).catch(() => console.err(err))
---
+++ services/passport.js
@@ -0,0 +1,26 @@
+const passport =require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+
+const keys = require('../config/keys')
+const User = mongoose.model('User')
+
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/api/auth/google/callback',
+  proxy: true,
+}, async(accessToken, refreshToken, profile, done) => {
+  console.log(accessToken);
+  console.log(refreshToken);
+  console.log(profile);
+
+  const existingUser = User.findOne({googleId: profile.id})
+
+  if(existingUser){
+    return done(null, existingUser)
+  }
+
+  const user = await new User({googleId: profile.id}).save()
+  done(null, user)
+}))
